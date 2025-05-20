616c5c061625a6aae0bdd359400bd2b1c8b30b03
Musk <yuiop1029@naver.com>
2025-05-20 13:35:06 +0900

    init

--- idfa/ContentView.swift
+++ idfa/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AdSupport
+import AppTrackingTransparency
 
 struct ContentView: View {
     var body: some View {
@@ -14,6 +16,34 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button {
+                let idfa = ASIdentifierManager.shared().advertisingIdentifier
+
+                // 콘솔에 출력
+                print("IDFA: \(idfa.uuidString)")
+            } label: {
+                Text("Test")
+            }
+            
+            Button {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                           DispatchQueue.main.async {
+                               switch status {
+                               case .authorized:
+                                   // 허용된 경우 IDFA 반환
+                                   let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                                  print("IDFA: \(idfa)")
+                               case .denied, .restricted, .notDetermined:
+                                   break
+                               @unknown default:
+                                  break
+                               }
+                           }
+                       }
+            } label: {
+                Text("Request Tracking")
+            }
         }
         .padding()
     }
