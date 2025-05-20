fbc9d1d1de59188bcf11de62004ef7b6b9bcf277
Musk <yuiop1029@naver.com>
2025-05-20 13:29:47 +0900

    Initial Commit

---
+++ idfa/idfaApp.swift
@@ -0,0 +1,17 @@
+//
+//  idfaApp.swift
+//  idfa
+//
+//  Created by yooinho on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct idfaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
